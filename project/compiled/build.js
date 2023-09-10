7e9ed7ca137ea974af9df7fff82f7aec27cb71b8
Eric Scott <escott@televet.com>
2023-09-10 01:52:11 -0500

    added sequencing

--- project/compiled/build.js
+++ project/compiled/build.js
@@ -1,1 +1,1 @@
-"use strict";
+"use strict";!function(e){e.fn.changeElementType=function(n){var t={};e.each(this[0].attributes,(function(e,n){t[n.nodeName]=n.nodeValue})),this.replaceWith((function(){return e("<"+n+"/>",t).append(e(this).contents())}))}}(jQuery);
